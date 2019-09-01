a5708652e6987faeb1ecc8a40eb115d37652c823
gasparpeter <petihappygaspar@gmail.com>
2019-09-01 02:48:10 -0700

    - useEffect

--- src/PostList.js
+++ src/PostList.js
@@ -1,21 +1,18 @@
 import React, {useState, useEffect} from 'react';
+import  axios from 'axios';
 
 export default () => {
 
-    const [list, setList] = useState([
-        {
-            "userId": 1,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-            "userId": 1,
-            "id": 2,
-            "title": "qui est esse",
-            "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-        }
-    ]);
+    const [list, setList] = useState([]);
+
+    const getList = async()=>{
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setList(data);
+    };
+
+    useEffect(()=>{
+        getList()
+    }, []);
 
     const renderPost = (post, index) => {
         return(
@@ -28,7 +25,6 @@ export default () => {
 
     return(
         <div>
-            Post List
             {list.length > 0 && list.map(renderPost)}
         </div>
     )
